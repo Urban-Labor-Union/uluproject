091e2c174a5db474342f5d16d1a1a9f2d3667160
Client Demo <ClientDemo@gep.com>
2019-06-29 16:25:54 +0530

    Purchase Order model

---
+++ ULUProject.Shared/PurchaseOrder.cs
@@ -0,0 +1,17 @@
+﻿namespace ULUApp.Shared
+{
+    public class PurchaseOrder
+    {
+        public int PurchaseOrderId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string BillingAddress { get; set; }
+
+        public string ShippingAddress { get; set; }
+
+        public string ContactPersonName { get; set; }
+    }
+}
