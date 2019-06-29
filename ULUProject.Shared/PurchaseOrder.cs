7d104b6968a57c0185362f91c3bfd38e9524c23d
Usama Ansari <usama251993@gmail.com>
2019-06-29 19:13:49 +0530

    Final Commit

---
+++ ULUProject.Shared/PurchaseOrder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using System.ComponentModel.DataAnnotations;
+
+namespace ULUProject.Shared
+{
+    public class PurchaseOrder
+    {
+        //[Required]
+        public int PurchaseOrderId { get; set; }
+
+        //[Required]
+        public string ProductName { get; set; }
+
+        //[Required]
+        public int Quantity { get; set; }
+
+        //[Required]
+        public string BillingAddress { get; set; }
+
+        //[Required]
+        public string ShippingAddress { get; set; }
+
+        //[Required]
+        public string ContactPersonName { get; set; }
+    }
+}
